53b7d3ea282a5390aee971cca4398872345537ea
JoshAmpofo <joshampofo92@gmail.com>
2023-01-18 15:56:13 +0000

    update function

--- monty_funcs_3.c
+++ monty_funcs_3.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 
-void rota(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
 void rotr(stack_t **stack, unsigned int line_number);
 void stack(stack_t **stack, unsigned int line_number);
 void queue(stack_t **stack, unsigned int line_number);
@@ -12,7 +12,7 @@ void queue(stack_t **stack, unsigned int line_number);
  *
  * Return: void
  */
-void rota(stack_t **stack, unsigned int line_number)
+void rotl(stack_t **stack, unsigned int line_number)
 {
 	stack_t *top, *bottom;
 
